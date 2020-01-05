fe194b58bc09dbf71c9c9f56118a2b44609c50ff
yuri.tsuprun@gmail.com <yurits@cybonet.local>
2020-01-05 10:44:22 +0200

    Initial commit

---
+++ OpsSchool-EKS.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = "us-east-1"
+}
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.10"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  cluster_name = "opsSchool-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+# CIDR will be "My IP" \ all Ips from which you need to access the worker nodes
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = "vpc-c17b2abb"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "209.88.185.5/32",
+      "207.232.13.77/32",
+      "192.168.1.0/32"
+    ]
+  }
+}
+
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = local.cluster_name
+  #TODO Ssbnet id
+  subnets      = ["subnet-0572a748", "subnet-18b1b744", "subnet-8b3035ec", "subnet-984a49b6"]
+
+  tags = {
+    Environment = "test"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = "vpc-c17b2abb"
+
+  # TODO Worker group 1
+  # One Subnet
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    }
+
+  ]
+
+}
